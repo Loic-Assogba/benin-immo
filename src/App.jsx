6a63ad78bd9a00c2bdd236afb72e335d4e694866
Loic-Assogba <loicassogba7@gmail.com>
2026-04-13 11:35:32 +0200

    init and setup benin-immo project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Header from "./components/sections/Header";
+function App() {
+  
+
+  return (
+    <main className="font-montserrat">
+      <Header />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+                <h1 className="text-3xl font-bold font-yeseva">Benin Immo</h1>
+        </header>
+    )
+}
