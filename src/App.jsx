7a0afdcce7e973d00dd087ba1a08a5bb83d909d7
Loic-Assogba <loicassogba7@gmail.com>
2026-05-22 23:55:19 +0100

    Insection de la section contact dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './components/sections/Header';
 import Hero from './components/sections/Hero';
 import Annonce from './components/sections/Annonce';
+import { Contact } from './components/sections/Contact';
+
 function App() {
     return (
         <main className="font-montserrat">
             <Header />
             <Hero />
             <Annonce />
+            <Contact />
         </main>
     );
 }
